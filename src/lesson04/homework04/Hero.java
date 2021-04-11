fcdb162bd133de5299d937de93aadcf9bab3f464
Neferriche <cancer0907@yandex.ru>
2021-04-11 19:12:35 +0300

    HW04

--- src/lesson04/homework04/Hero.java
+++ src/lesson04/homework04/Hero.java
@@ -1,2 +1,46 @@
-package lesson04.homework04;public class Hero {
+package lesson04.homework04;
+
+public class Hero {
+    String name;
+    int age;
+    String weapon;
+    int power;
+    int speed;
+    int intellect;
+
+    public Hero (String name, int age, String weapon, int power, int speed, int intellect) {
+        this.name = name;
+        this.age = age;
+        this.weapon = weapon;
+        this.power = power;
+        this.speed = speed;
+        this.intellect = intellect;
+    }
+
+    void hello() {
+        System.out.println("- Hello, my name is " + name + "!");
+    }
+
+    void myAge() {
+        System.out.println("- I'm " + age + ".");
+    }
+
+    void myWeapon() {
+        System.out.println("- And as you see, I fight with my " + weapon + ".");
+    }
+
+    void myPower() {
+        System.out.println("Power: " + power);
+    }
+
+    void mySpeed() {
+        System.out.println("Speed: " + speed);
+    }
+
+    void myIntellect() {
+        System.out.println("Intellect: " + intellect);
+    }
+
+
+
 }
--- src/lesson04/homework04/Homework04.java
+++ src/lesson04/homework04/Homework04.java
@@ -1,2 +1,30 @@
-package lesson04.homework04;public class Homework04 {
+package lesson04.homework04;
+
+public class Homework04 {
+
+    public static <heroes> void main(String[] args) {
+
+        Hero[] heroes = new Hero[5];
+        heroes[0] = new Hero("Donatello", 14, "Tech-bo", 7, 4, 10);
+        heroes[1] = new Hero("Splinter", 45, "Stick", 9, 8, 6);
+        heroes[2] = new Hero("April", 16, "Bat", 8, 6, 7);
+        heroes[3] = new Hero("Shredder", 542, "Claws", 10, 9, 6);
+        heroes[4] = new Hero("Draxum", 610, "Vines",9, 6, 10);
+
+        heroes[0].hello();
+        heroes[0].myAge();
+        heroes[0].myWeapon();
+//        System.out.println("Look at my characteristics:");
+//        heroes[0].myPower();
+//        heroes[0].mySpeed();
+//        heroes[0].myIntellect();
+
+        System.out.println();
+
+        for (int i = 0; i < heroes.length; i++) {
+            if(heroes[i].age > 500) {
+                System.out.println(heroes[i].name + " is more than 500 years old.");
+            }
+        }
+    }
 }
